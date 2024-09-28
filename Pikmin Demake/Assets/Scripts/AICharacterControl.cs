b374a4d06a2a8bd5f89a5fd7e0ad322ece62f87e
GodOfImagination <kurzawa.jaden@gmail.com>
2024-09-28 16:15:53 -0700

    Messing Around

---
+++ Pikmin Demake/Assets/Scripts/AICharacterControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AICharacterControl : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public Transform _target;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (_target != null)
+            _agent.SetDestination(_target.position);
+    }
+}

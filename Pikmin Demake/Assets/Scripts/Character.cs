da1815abab2dd846f431cc7dc2d46b3601a56fdb
GodOfImagination <kurzawa.jaden@gmail.com>
2024-09-28 17:44:20 -0700

    Importing Lecture Scripts

---
+++ Pikmin Demake/Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Character : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+            {
+                Debug.Log("Left Clicked: " + hit.point);
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+}

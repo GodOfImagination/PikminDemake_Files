43a0a38ad6fcf5cb83ec791e15e34266b59c9862
GodOfImagination <kurzawa.jaden@gmail.com>
2024-09-29 23:26:27 -0700

    Figured it Out + Obstacles
    Thank god for that. I was ready to snap at any given moment thanks to that fucking bug.

--- Pikmin Demake/Assets/Scripts/Character.cs
+++ Pikmin Demake/Assets/Scripts/Character.cs
@@ -5,18 +5,18 @@ using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
-    private NavMeshAgent CharacterAgent;
-    private LineRenderer CharacterLine;
+    private NavMeshAgent CharacterAgent;       // The Character's Navigation.
+    private LineRenderer CharacterLine;        // The Character's Indicator.
 
-    public bool CharacterSelected = false;
-    public bool CharacterMoving = false;
-    public bool CanClick = true;
+    public bool CharacterSelected = false;     // Tells whether or not the character has been selected.
+    public bool CharacterMoving = false;       // Tells whether or not the character is moving.
+    public bool CanClick = true;               // Tells whether or not the player is allowed to click.
 
     void Start()
     {
         CharacterAgent = GetComponent<NavMeshAgent>();
+        CharacterLine = GetComponent<LineRenderer>();
 
-        CharacterLine = gameObject.AddComponent<LineRenderer>();
         CharacterLine.startWidth = 0.2f;
         CharacterLine.endWidth = 0.2f;
         CharacterLine.positionCount = 2;
@@ -27,7 +27,7 @@ public class Character : MonoBehaviour
     {
         Vector3 MousePosition = Input.mousePosition;
         Ray CastPoint = Camera.main.ScreenPointToRay(MousePosition);
-        RaycastHit Hit = new RaycastHit();
+        RaycastHit HitInfo;
 
         CharacterLine.SetPosition(0, transform.position);
 
@@ -43,9 +43,9 @@ public class Character : MonoBehaviour
             }
         }
 
-        if (Physics.Raycast(CastPoint, out Hit, Mathf.Infinity) && CharacterMoving == false)
+        if (Physics.Raycast(CastPoint, out HitInfo, Mathf.Infinity) && CharacterMoving == false)
         {
-            CharacterLine.SetPosition(1, Hit.point);
+            CharacterLine.SetPosition(1, HitInfo.point);
         }
 
         if (Input.GetMouseButtonDown(0) && CharacterSelected && CanClick)
@@ -53,11 +53,11 @@ public class Character : MonoBehaviour
             CharacterMoving = true;
             CanClick = false;
 
-            if (Physics.Raycast(CastPoint, out Hit, Mathf.Infinity))
+            if (Physics.Raycast(CastPoint, out HitInfo, Mathf.Infinity))
             {
-                Debug.Log("Clicked: " + Hit.point);
-                CharacterAgent.SetDestination(Hit.point);
-                CharacterLine.SetPosition(1, Hit.point);
+                Debug.Log("Clicked: " + HitInfo.point);
+                CharacterAgent.SetDestination(HitInfo.point);
+                CharacterLine.SetPosition(1, HitInfo.point);
             }
         }
     }
--- Pikmin Demake/Assets/Scripts/Gameplay.cs
+++ Pikmin Demake/Assets/Scripts/Gameplay.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Gameplay : MonoBehaviour
 {
-    private Camera MainCamera;
+    private Camera MainCamera;                  // The Game's Camera.
+    private bool CharacterSelected = false;     // Tells whether or not a character has been selected.
+    private bool IsLooking = false;             // Set to true when free looking (on right mouse button).
 
-    public int CameraMoveSpeed;
+    [Header("Camera\n")]
+    public float NormalSpeed = 10f;             // Speed of camera movement.
+    public float FastSpeed = 100f;              // Speed of camera movement when shift is held down.
+    public float LookSensitivity = 3f;          // Sensitivity for free look.
+    public float NormalSensitivity = 10f;       // Amount to zoom the camera when using the mouse wheel.
+    public float FastSensitivity = 50f;         // Amount to zoom the camera when using the mouse wheel (fast mode).
+
+    [Header("Text\n")]
+    public TextMeshProUGUI CharacterText;       // Tells the player if they have a specific character selected or not.
+    public TextMeshProUGUI GoalText;            // Tells the player if they have all characters at the goal.
 
     void Start()
     {
@@ -21,20 +33,37 @@ public class Gameplay : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(CastPoint, out HitInfo))
+            if (Physics.Raycast(CastPoint, out HitInfo, Mathf.Infinity))
             {
                 Character ClickedCharacter = HitInfo.transform.GetComponent<Character>();
 
-                if (ClickedCharacter != null)
+                if (ClickedCharacter != null && CharacterSelected == false)
                 {
                     Debug.Log("Selected Character");
                     ClickedCharacter.Selected();
+                    CharacterSelected = true;
+
+                    if (ClickedCharacter.tag == "Red")
+                    {
+                        CharacterText.text = "Red";
+                        CharacterText.color = new Color(255, 0, 0);
+                    }
+                    else if (ClickedCharacter.tag == "Yellow")
+                    {
+                        CharacterText.text = "Yellow";
+                        CharacterText.color = new Color(255, 255, 0);
+                    }
+                    else if (ClickedCharacter.tag == "Blue")
+                    {
+                        CharacterText.text = "Blue";
+                        CharacterText.color = new Color(0, 0, 255);
+                    }
                 }
             }
         }
         else if (Input.GetMouseButtonDown(1))
         {
-            if (Physics.Raycast(CastPoint, out HitInfo))
+            if (Physics.Raycast(CastPoint, out HitInfo, Mathf.Infinity))
             {
                 Character ClickedCharacter = HitInfo.transform.GetComponent<Character>();
 
@@ -42,28 +71,106 @@ public class Gameplay : MonoBehaviour
                 {
                     Debug.Log("Unselected Character");
                     ClickedCharacter.Unselected();
+                    CharacterSelected = false;
+
+                    CharacterText.text = "None";
+                    CharacterText.color = new Color(0, 0, 0);
                 }
             }
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        /// Keys:
+        ///	WASD / Arrows	    - Movement
+        ///	Q / E 			    - Up / Down (Local Space)
+        ///	R / F 			    - Up / Down (World Space)
+        ///	PageUp / PageDown	- Up / Down (World Space)
+        ///	Hold Shift		    - Enable Fast Movement Mode
+        ///	Right Mouse  	    - Enable Free Look
+        ///	Mouse			    - Free Look / Rotation
+
+        var FastMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        var MovementSpeed = FastMode ? FastSpeed : NormalSpeed;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            MainCamera.transform.Translate(Vector3.left * CameraMoveSpeed * Time.deltaTime);
+            MainCamera.transform.position = MainCamera.transform.position + (-MainCamera.transform.right * MovementSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            MainCamera.transform.Translate(Vector3.right * CameraMoveSpeed * Time.deltaTime);
+            MainCamera.transform.position = MainCamera.transform.position + (MainCamera.transform.right * MovementSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            MainCamera.transform.Translate(Vector3.forward * CameraMoveSpeed * Time.deltaTime);
+            MainCamera.transform.position = MainCamera.transform.position + (MainCamera.transform.forward * MovementSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            MainCamera.transform.Translate(-Vector3.forward * CameraMoveSpeed * Time.deltaTime);
+            MainCamera.transform.position = MainCamera.transform.position + (-MainCamera.transform.forward * MovementSpeed * Time.deltaTime);
         }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            MainCamera.transform.position = MainCamera.transform.position + (MainCamera.transform.up * MovementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            MainCamera.transform.position = MainCamera.transform.position + (-MainCamera.transform.up * MovementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp))
+        {
+            MainCamera.transform.position = MainCamera.transform.position + (Vector3.up * MovementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown))
+        {
+            MainCamera.transform.position = MainCamera.transform.position + (-Vector3.up * MovementSpeed * Time.deltaTime);
+        }
+
+        if (IsLooking)
+        {
+            float NewRotationX = MainCamera.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * LookSensitivity;
+            float NewRotationY = MainCamera.transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * LookSensitivity;
+            MainCamera.transform.localEulerAngles = new Vector3(NewRotationY, NewRotationX, 0f);
+        }
+
+        float Axis = Input.GetAxis("Mouse ScrollWheel");
+        if (Axis != 0)
+        {
+            var zoomSensitivity = FastMode ? FastSensitivity : NormalSensitivity;
+            MainCamera.transform.position = MainCamera.transform.position + MainCamera.transform.forward * Axis * zoomSensitivity;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            StopLooking();
+        }
+    }
+
+    void OnDisable()
+    {
+        StopLooking();
+    }
+
+    public void StartLooking()     // Enable free looking.
+    {
+        IsLooking = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void StopLooking()      // Disable free looking.
+    {
+        IsLooking = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
